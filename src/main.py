0b949094e577bdd19b642fe6e851b0e3fe6a3130
Ward Franssen <franssenward3@gmail.com>
2024-10-25 14:34:36 +0200

    Test

--- src/main.py
+++ src/main.py
@@ -1,4 +1,27 @@
+import json
+from git import Repo
+from git import Actor
+
+static_folder = "..\\static"
+
+with open(f"{static_folder}\\letters.json", "r") as file:
+    letters_json = json.loads(file.read())
 
 
 def text_to_blocks(text: str):
-    print()
+    print(letters_json["letters"]["A"])
+    for letter in text.upper():
+        print()
+
+
+def make_commit(repo_dir):
+    with open(f"{static_folder}\\commit_message.txt") as file:
+        commit_message = file.read()
+
+    repo = Repo(repo_dir)
+    repo.index.add(f"C:\\Users\\wardf\\PycharmProjects\\gitCommitHistory\\static\\commit.txt")
+    repo.index.commit(commit_message, author=Actor("Python", ""))
+
+
+dir = input("Dir of repo: ")
+make_commit(dir)
