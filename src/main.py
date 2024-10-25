2a59f6f7441e015c8650659975c03b7dbeaa795b
wardfranssen <ward.f.franssen@gmail.com>
2024-10-25 23:23:13 +0200

    First test + new letter size Now have small and big letters Ran first test with current main.py

--- src/main.py
+++ src/main.py
@@ -1,65 +1,45 @@
+from datetime import datetime
 import json
-from git import Repo
-from git import Actor
-import requests
-import subprocess
 import os
+import subprocess
+import time
 
 static_folder = "..\\static"
 
 with open(f"{static_folder}\\letters.json", "r") as file:
     letters_json = json.loads(file.read())
 
-
-def text_to_blocks(text: str):
-    print(letters_json["letters"]["A"])
-    for letter in text.upper():
-        print()
-
-
-def make_commit(repo_dir):
-    with open(f"{static_folder}\\commit_message.txt") as file:
-        commit_message = file.read()
-
-    repo = Repo(repo_dir)
-    repo.index.add(f"C:\\Users\\wardf\\PycharmProjects\\gitCommitHistory\\static\\commit.txt")
-    repo.index.commit(commit_message, author=Actor("Python", ""))
-    # repo.index.
+days_of_the_week = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+]
 
 
-def get_header(owner: str, repo_name: str, branch_name: str):
-    response = requests.get(f"https://api.github.com/repos/{owner}/{repo_name}/git/ref/heads/{branch_name}")
+def letter_to_dates(letter: str, year: int) -> list:
+    global day
 
-    print(response.json())
+    dates = []
+    for column in zip(*letter):
+        for box in column:
+            if box == "X":
+                if day-first_day_of_year >= 0:
+                    try:
+                        # Turn day number into date of given year
+                        dates.append(datetime.strptime(f"{year}-{day-first_day_of_year+1}", "%Y-%j").strftime("%Y-%m-%d"))
+                    except ValueError:
+                        print("Text is too long, it does not fit in 1 year")
+                        raise exit(0)
+            day += 1
 
-    url = response.json()["object"]["url"]
-    response = requests.get(url)
-
-    commit_sha = response.json()["sha"]
-    tree_sha = response.json()["tree"]["sha"]
-    tree_url = response.json()["tree"]["url"]
-
-    print(commit_sha, tree_sha, tree_url)
-
-    payload = {
-        "content": "SGVsbG8sIFdvcmxkIQ==",
-        "encoding": "base64"
-    }
-
-    response = requests.post(f"https://api.github.com/repos/{owner}/{repo_name}/git/blobs", data=payload)
-    print(response.json())
+    return dates
 
 
-# username = input("Github username: ")
-# repo_name = input("Repo name: ")
-# branch_name = input("Branch name: ")
-# get_header(username, repo_name, branch_name)
-
-
-
-
-def git_commit_push(repo_path, message, branch="main"):
-    # Check if the repository path exists
+def git_commit_push(repo_path: str, message: str, date="", branch="main") -> None:
     if not os.path.isdir(repo_path):
         print(f"Error: The specified repository path '{repo_path}' does not exist.")
         return
@@ -71,8 +51,12 @@ def git_commit_push(repo_path, message, branch="main"):
         # Stage all changes
         subprocess.run(["git", "add", "."], check=True)
 
+        commit_command = ["git", "commit", "-m", message, "--allow-empty"]
+
+        if date:
+            commit_command.append(f"--date={date}")
         # Commit changes with the provided message
-        subprocess.run(["git", "commit", "-m", message, "--allow-empty"], check=True)
+        subprocess.run(commit_command, check=True)
 
         # Push the changes to the specified branch on the remote repository
         subprocess.run(["git", "push", "origin", branch], check=True)
@@ -82,10 +66,32 @@ def git_commit_push(repo_path, message, branch="main"):
         print(f"An error occurred: {e}")
 
 
-# Usage example
-repo_path = input("Enter the path to your repository: ")
-commit_message = input("Enter your commit message: ")
-git_commit_push(repo_path, commit_message)
+if __name__ == "__main__":
+    repo_dir = input("Directory of repo: ")
+    commit_message = input("Commit message: ")
 
+    text = input("Text: ")
+    text_size = input("What size should the text be (big/small): ")
+    year = int(input("What year should your commits be in: "))
 
-# make_commit("C:\\Users\\wardf\\PycharmProjects\\gitCommitHistory")
+    # Get week day of first day of given year and turn it into index
+    first_day_of_year = days_of_the_week.index(datetime(year, 1, 1).strftime("%A"))
+
+    dates = []
+    day = 0
+
+    for letter in text.strip().upper():
+        # +14 is 3 weeks because it will also add 1 week after each letter (the letter before the space)
+        if letter == " ":
+            day += 14
+            continue
+        else:
+            dates.append(letter_to_dates(letters_json["letters"][text_size][letter], year))
+
+        # Add some space between each letter
+        day += 7
+
+    for row in dates:
+        for date in row:
+            git_commit_push(repo_dir, commit_message, date=date)
+            time.sleep(1)
